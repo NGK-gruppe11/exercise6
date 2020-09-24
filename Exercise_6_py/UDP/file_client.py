723040c1919a70677de755daeea0bc1aaac84e8d
ase <m.vraa@live.dk>
2020-09-24 11:10:56 +0200

    udp virker

--- Exercise_6_py/UDP/file_client.py
+++ Exercise_6_py/UDP/file_client.py
@@ -1,10 +1,12 @@
+#!/usr/bin/python3
+
 import sys
 from socket import socket, AF_INET, SOCK_DGRAM
 
 PORT = 9000
 
 def main(argv):
-	NAME = "10.0.0.1"
+	NAME = "192.168.199.137"
 
 	print("Klient connecter...")
 	clientSocket = socket(AF_INET, SOCK_DGRAM)
@@ -13,11 +15,11 @@ def main(argv):
 	clientSocket.sendto(msgToServer.encode(), (NAME, PORT))
 	bytesAdrPair = clientSocket.recvfrom(2048)
 	
-	msgFromServer = vytesAdrPair[0]
+	msgFromServer = bytesAdrPair[0]
 	print(msgFromServer.decode())
 	clientSocket.close()
     
-def receiveFile(fileName,  conn):
+#def receiveFile(fileName,  conn):
 	# TO DO Your Code
 
 if __name__ == "__main__":

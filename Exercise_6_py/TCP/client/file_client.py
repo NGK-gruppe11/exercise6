00197655f48376cb4e3eaf77b0a8bc0a2b4cc233
mvraa <40834090+mvraa@users.noreply.github.com>
2020-10-22 17:10:20 +0200

    tilfæjede kommentarer

--- Exercise_6_py/TCP/client/file_client.py
+++ Exercise_6_py/TCP/client/file_client.py
@@ -5,7 +5,7 @@ import socket
 from lib import Lib
 
 HEADER = 1000
-SERVER = "192.168.8.101"
+SERVER = "192.168.8.101" # ip på server
 PORT = 5050
 ADDR = (SERVER, PORT)
 
@@ -13,24 +13,24 @@ def main(argv):
 	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	print("Klient connecter til ", ADDR)
 
-	client.connect(ADDR)
+	client.connect(ADDR) # connect til server
 	print("Klient connected.")
-	fileMsg = input("Input: ")
+	fileMsg = input("Input: ") # få terminal input (fil)
 
 	# file size
-	client.sendto(fileMsg.encode(), ADDR)
-	bytesAdrPair = client.recvfrom(HEADER)
-	msgFromServer = bytesAdrPair[0]
-	print(msgFromServer.decode())
+	client.sendto(fileMsg.encode(), ADDR) # send navn på fil. encode() konvertere besked til bits
+	bytesAdrPair = client.recvfrom(HEADER) # modtag besked fra server (size af fil)
+	msgFromServer = bytesAdrPair[0] # selve besked ligger på index 0
+	print(msgFromServer.decode()) # print size af fil (eller fejl besked)
 
 	# receive file
-	with open(fileMsg, "wb") as file:
+	with open(fileMsg, "wb") as file: # lav ny fil på klient
 		print("Getting file...")
 		while True:
-			data = client.recv(HEADER)
-			file.write(data)
-			if not data:
-				break
+			data = client.recv(HEADER) # modtag besked 1000 bytes ad gangen
+			file.write(data) # skriv modtaget data til egen fil
+			if not data: # hvis der ikke er mere data...
+				break # bryd loop
 
 		print("File received.")
 	client.close()
--- Exercise_6_py/TCP/server/file_server.py
+++ Exercise_6_py/TCP/server/file_server.py
@@ -2,49 +2,51 @@
 
 import sys
 import socket
-from lib import Lib
+from lib import Lib # har en funktion til at returnere size af fil
 
-HEADER = 1000
+HEADER = 1000 # bytes der skal sendes pr gang
 SERVER = "192.168.8.101" # local ip
 PORT = 5050
-ADDR = (SERVER, PORT)
+ADDR = (SERVER, PORT) # adresse består af ip + port
 FORMAT = 'utf-4'
 
 def main(argv):
 
 	print("Server set to ", ADDR)
-	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # setup server som ipv4 og tcp
 
-	server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # ingen ventetid når server genstartes
 
-	server.bind(ADDR)
-	server.listen()
+	server.bind(ADDR) # bind addresse og server sammen
+	server.listen() # lyt efter klienter
 
 	while 1:
 		print("Server er klar.")
-		conn, addr = server.accept()
-		print("Socket accept", addr)
+		conn, addr = server.accept() # blokerende funktion der venter indtil listen() opfanger en klient
+		print("Socket accept", addr) # print klient
 
-		msg = conn.recv(HEADER)
-		print("Besked modtaget fra klient:", msg.decode())
+		msg = conn.recv(HEADER) # blokerende linje der venter til besked er modtaget (fil navnet)
+		print("Besked modtaget fra klient:", msg.decode()) # decode() konvertere besked til string
 		
-		sendFile(msg.decode(), conn)
+		sendFile(msg.decode(), conn) # send fil (msg) til klient(conn)
 
 		conn.close()
 
 def sendFile(fileName, conn):
 	try:
+		# prøv at sende størrelse af fil...
 		msg = "File size: " + str(Lib.check_File_Exists(fileName))
 		conn.send(msg.encode())
 	except:
+		# hvis det mislykkes. send en fejl besked
 		conn.send("File not found!".encode())
 
-	with open(fileName, "rb") as file:
-		data = file.read(HEADER)
+	with open(fileName, "rb") as file: # åben fil
+		data = file.read(HEADER) # læs de første 1000 bytes
 		print("Sending...")
-		while data:
-			conn.send(data)
-			data = file.read(HEADER)
+		while data: # læs data indtil der ikke er mere
+			conn.send(data) # send til klient
+			data = file.read(HEADER) # læs næste 1000 bytes
 	print("File sent.")
 
 if __name__ == "__main__":
